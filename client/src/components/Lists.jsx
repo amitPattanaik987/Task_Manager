0a69670fe78e8ebfd8bae942a149959af766f445
amitPattanaik987 <amitpattanaik987@gmail.com>
2024-08-26 21:56:06 +0530

    Third-commit

--- client/src/components/Lists.jsx
+++ client/src/components/Lists.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useContext } from 'react';
 import { itemsContext } from '../Store/Store';
+import "./Lists.css"
 
 function Lists() {
   const navigate = useNavigate();
@@ -73,14 +74,14 @@ function Lists() {
 
   return (
     <div className='Outer-box flex justify-center items-center h-screen'>
-      <div className='w-[50%] h-[70%] bg-white rounded-lg flex xl:p-[20px] max-xl:p-[15px] max-lg:p-[10px] max-sm:p-[5px] max-lg:w-[70%] max-lg:h-[80%] max-sm:w-[95%] max-sm:h-[85%]'>
-        <div className='bg-white w-[35%] p-[5px] flex flex-col justify-between h-[100%] max-xl:p-[8px]'>
-          <div className='flex flex-col gap-[3px] bg-white max-xl:gap-[2px]'>
+      <div className='w-[70%] h-[70%] bg-white rounded-lg flex xl:p-[20px] max-xl:p-[15px] max-lg:p-[10px] max-sm:p-[5px] max-lg:w-[70%] max-sm:w-[95%]'>
+        <div className='bg-white w-[35%] p-[5px] flex flex-col justify-between max-xl:p-[8px] left-container'>
+          <div className='flex flex-col gap-[3px] bg-white max-xl:gap-[2px] left-container-upper'>
             <p className='bg-white text-sky-600 text-[18px] flex max-xl:text-[17px] justify-between p-[4px] max-sm:p-[3px] max-sm:text-[15px] max-sm:flex max-sm:flex-col max-sm:text-center'>LISTS<span className='bg-white'>{Lists1.length != 0 ? <p className='text-[15px] text-center bg-white text-red-500 max-xl:text-[13px]'>(DOUBLECLICK TO DELETE)</p> : null}</span></p>
-            <div className='flex flex-col gap-[3px] bg-white p-[5px] h-[300px] overflow-y-auto max-lg:h-[350px] max-sm:h-[370px]'>
+            <div className='flex flex-col gap-[3px] bg-white p-[5px] overflow-y-auto list-data'>
               {Lists1.length > 0 ? (
                 Lists1.map((item) => (
-                  <p key={item._id} className='p-[4px] flex justify-center cursor-pointer text-center select-none max-xl:h-[35px] max-sm:text-[13px] max-sm:p-[2px]' onClick={event} onDoubleClick={doubleclick}>{item.List}</p>
+                  <p key={item._id} className='p-[4px] flex justify-center cursor-pointer text-center select-none max-xl:h-[35px] max-sm:text-[13px] max-sm:p-[2px] inner-list' onClick={event} onDoubleClick={doubleclick}>{item.List}</p>
                 ))
               ) : (
                 <p className='flex text-center justify-center bg-orange-300 text-3xl p-[20px]'>No lists available !!</p>
@@ -103,18 +104,18 @@ function Lists() {
                   <p className='bg-white text-3xl'>Click on the lists to add tasks</p>
                 </div>
                 :
-                <div className='bg-white flex flex-col'>
+                <div className='bg-white flex flex-col right-main-container'>
                   <div className='bg-white flex justify-between items-center mb-4'>
                     <p className='bg-white text-3xl text-sky-600 p-2 max-sm:text-[20px]'>TASKS-{clickedelement}</p>
                     <button className='btn btn-success' onClick={Addbtnclicked}>+</button>
                   </div>
-                  <div className='flex flex-col gap-1 bg-white p-2 h-[320px] overflow-y-auto max-sm:h-[350px]'>
+                  <div className='flex flex-col gap-1 bg-white p-2 overflow-y-auto tasks-data'>
                     {
                       tasks.length === 0 ?
                         <p className='bg-white text-center p-2'>There Is No Tasks! Click On The Add Button To Create Tasks.</p> :
                         tasks.map((item) => (
                           <div key={Math.random()} className='p-[10px] bg-slate-300 border border-slate-400 flex justify-between'>
-                            <p className='bg-slate-300 w-[100%] flex items-center p-[5px]'>{item}</p>
+                            <p className='bg-slate-300 w-[100%] flex items-center p-[5px] justify-center'>{item}</p>
                             <button className='btn btn-danger' onClick={() => {
                               deleteclicked(item);
                             }}>🗑️</button>
